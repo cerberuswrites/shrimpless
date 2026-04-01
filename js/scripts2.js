9480dfc946a2e7e4b1ce2b28b1347f7a9f28f19c
shrimpless <34462778+cerberuswrites@users.noreply.github.com>
2026-04-01 16:20:28 +1100

    added regex helper for zugzy

---
+++ js/scripts2.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+  function mreplace(replacements, str) {
+    let result = str
+    for (let [x, y] of replacements) result = result.replace(x, y)
+    return result
+  }
+    
+  $("button.replaceTool").click(function () {
+    
+    let input = $(".input").val()
+    let output = mreplace([
+        [/\*{2}(.*?)\*{2}/gm, "[b]$1[/b]"],
+        [/\*{1}(.*?)\*{1}/gm, "[i]$1[/i]"],
+        [/^(.*: )\*(.*)(?!\*)$/gm, "$1[i]$2[/i]"],
+        [/^M:/gm, "MILO:"],
+        [/^S:/gm, "SABLE:"],
+        [/^B:/gm, "BIANCA:"],
+        [/i'm/gm, "I'm"],
+        [/[i](?!\S)/gm, "I"],
+      ], input, );
+    
+    if ($('input#narration-tag').is(':checked')) {
+        output = output.replace(/\n(?!MILO|SABLE|BIANCA)(\w)/gm, "\nMILO_N: $1")
+    }
+    
+    var regexArray = [/(?<=\. )(\w)/gm, /^(\w)/gm, /(?<=MILO_N: )(\w)/gm, /(?<=: \[i\])(\w)/gm]
+    $.each(regexArray, function( i, regex ) {
+      output = output.replace(regex, function(x){return x.toUpperCase();});
+    });
+
+    // trim extra whitespace before closing italic tags
+    output = output.replace(/\s(?=\[\/i\]$)/gm, "")
+
+    $(".res2").text(output);
+
+  });
+
+  new ClipboardJS(".btn")
+
+})
